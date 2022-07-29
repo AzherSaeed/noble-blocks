958447944e55417101ec0c78bdf14bfa53f95a6c
TalhaMW <talhamuneer101@gmail.com>
2022-07-29 07:27:07 -0700

    team section complete

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,6 +6,15 @@ import trustworthIcon from "./icons/trustworthIcon.svg";
 // homepage About Noble icons
 import openAcces from "./icons/openAccess.svg";
 
+// home page images
+
+import elfieSolomon from "./img/elfieSolomon.png";
+import elfie2 from "./img/elfie2.png";
+import adamSkull from "./img/adamSkull.png";
+import holyMichael from "./img/holyMichael.png";
+import michaelGray from "./img/michaelGray.png";
+import delroy from "./img/delroy.jpeg";
+import bentey from "./img/bentey.jpeg";
 export {
   // home page icons
   efficentIcon,
@@ -13,4 +22,12 @@ export {
   trustworthIcon,
   // home page About Noble Access
   openAcces,
+  // home page images
+  elfieSolomon,
+  holyMichael,
+  elfie2,
+  michaelGray,
+  adamSkull,
+  delroy,
+  bentey,
 };
